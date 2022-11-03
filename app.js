0662d09e3505c46e3d04145ae3852c568fad5a3a
Ashley Capace <acapace@yahoo.com>
2022-11-03 17:40:59 -0400

    update

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
 
 $(() => {
+
+        $(".home").click(function(){
+        $(".home > p").toggle();
+          $(".about > p").css("display", "none");
+          $(".resume > p").css("display", "none");
+          $(".projects > p").css("display", "none");
+    }); 
+       $(".about").click(function(){
+        $(".about > p").toggle();
+          $(".home > p").css("display", "none");
+          $(".resume > p").css("display", "none");
+          $(".projects > p").css("display", "none");
+
+   });
+       $(".resume").click(function(){
+       $(".resume > p").toggle();
+         $(".about > p").css("display", "none");
+         $(".home > p").css("display", "none");
+         $(".projects > p").css("display", "none");
+   });
+       $(".projects").click(function(){
+       $(".projects > p").toggle();
+         $(".about > p").css("display", "none");
+         $(".home > p").css("display", "none");
+         $(".resume > p").css("display", "none");
+   });
+ });
+
+
+
