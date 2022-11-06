af340c2debcbea4e897442148d4e5bfdf66c8534
Ashley Capace <acapace@yahoo.com>
2022-11-06 09:56:12 -0500

    update

--- app.js
+++ app.js
@@ -3,31 +3,31 @@ $(() => {
 
 ////buttons with a drop down
 
-        $(".home").click(function(){
-        $(".home > p").toggle();
-          $(".about > p").css("display", "none");
-          $(".resume > p").css("display", "none");
-          $(".projects > p").css("display", "none");
-    }); 
-       $(".about").click(function(){
-        $(".about > p").toggle();
-          $(".home > p").css("display", "none");
-          $(".resume > p").css("display", "none");
-          $(".projects > p").css("display", "none");
+        // $(".home").click(function(){
+        // $(".home > p").toggle();
+//           $(".about > p").css("display", "none");
+//           $(".resume > p").css("display", "none");
+//           $(".projects > p").css("display", "none");
+//     }); 
+    //    $(".about").click(function(){
+        // $(".about > p").toggle();
+//           $(".home > p").css("display", "none");
+//           $(".resume > p").css("display", "none");
+//           $(".projects > p").css("display", "none");
 
-   });
-       $(".resume").click(function(){
-       $(".resume > p").toggle();
-         $(".about > p").css("display", "none");
-         $(".home > p").css("display", "none");
-         $(".projects > p").css("display", "none");
-   });
-       $(".projects").click(function(){
-       $(".projects > p").toggle();
-         $(".about > p").css("display", "none");
-         $(".home > p").css("display", "none");
-         $(".resume > p").css("display", "none");
-   });
+//    });
+//        $(".resume").click(function(){
+//        $(".resume > p").toggle();
+//          $(".about > p").css("display", "none");
+//          $(".home > p").css("display", "none");
+//          $(".projects > p").css("display", "none");
+//    });
+//        $(".projects").click(function(){
+//        $(".projects > p").toggle();
+//          $(".about > p").css("display", "none");
+//          $(".home > p").css("display", "none");
+//          $(".resume > p").css("display", "none");
+//    });
 
 
 ////footer - mouseover mouseout for links
